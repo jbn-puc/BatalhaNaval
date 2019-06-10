f75ca6ef7de6a1afb6589b0653ad7ea01f2170df
Bruno Silva Freire <bruno@gmail.com>
2019-06-10 09:08:49 -0300

    aaa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,40 +3,71 @@ using Lunari.Tsuki;
 using Lunari.Tsuki.Singletons;
 using UnityEngine;
 
-public enum GameState {
+public enum GameState
+{
     PlacingPlayer1,
     PlacingPlayer2,
     Player1,
     Player2
 }
 
-public class Field {
+public class Field
+{
     public GridSlot[,] Grid { get; private set; }
-
-    public Field(GameManager manager) {
-        var width = manager.width;
-        var height = manager.height;
+    public byte width, height;
+    public Field(GameManager manager)
+    {
+        width = manager.width;
+        height = manager.height;
         var layout = manager.layout;
         var slotPrefab = manager.slotPrefab;
         Grid = new GridSlot[width, height];
-        for (var x = 0; x < width; x++) {
-            for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            { 
                 var obj = Grid[x, y] = Object.Instantiate(slotPrefab, layout.transform);
+                obj.isExposed = true;
                 obj.transform.position = layout.GetCellCenterWorld(new Vector3Int(x, y, 0));
+            }
+        }
+        foreach (var unit in
+        manager.deck.availableUnits)
+        {
+            bool placed = false;
+            while (!placed)
+            {
+                var baseX = Random.Range(0, width);
+                var baseY = Random.Range(0, height);
+                if (unit.structure.CanPlaceIn(this, baseX, baseY))
+                {
+                    for (var x = 0; x < unit.structure.width; x++)
+                    {
+                        for (var y = 0; y < unit.structure.height; y++)
+                        {
+                            Grid[baseX + x, baseY + y].unit = unit;
+                        }
+                    }
+                    Debug.Log(baseX + " -> " + (baseX + unit.structure.width)+", "+ baseY + " -> " + (baseY + unit.structure.height)+ ": "+unit);
+                    placed = true;
+                }
             }
         }
     }
 
-    public void SetVisible(bool visible) {
-        foreach (var slot in Grid) {
+    public void SetVisible(bool visible)
+    {
+        foreach (var slot in Grid)
+        {
             slot.gameObject.SetActive(visible);
         }
     }
 }
 
-public class GameManager : Singleton<GameManager> {
+public class GameManager : Singleton<GameManager>
+{
     public GridSlot slotPrefab;
-    private Field player1, player2;
+    public Field player1, player2;
     public Grid layout;
     public GameState state;
     public byte width, height;
@@ -45,22 +76,28 @@ public class GameManager : Singleton<GameManager> {
     public Deck deck;
     private Coroutine awaitRoutine;
 
-    private void Update() {
+    private void Update()
+    {
         var c = Camera.main;
-        if (c == null) {
+        if (c == null)
+        {
             return;
         }
 
         var ray = c.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (!Physics.Raycast(ray, out hit)) {
+        if (!Physics.Raycast(ray, out hit))
+        {
             return;
         }
 
         var wp = hit.point;
-        selected = (Vector2Int) layout.WorldToCell(wp);
-        if ((state == GameState.Player1 || state == GameState.Player2) && allowInput) {
-            if (!Input.GetMouseButtonDown(0)) {
+        selected = (Vector2Int)layout.WorldToCell(wp);
+        if ((state == GameState.Player1 || state == GameState.Player2) && allowInput)
+        {
+
+            if (!Input.GetMouseButtonDown(0))
+            {
                 return;
             }
 
@@ -70,10 +107,12 @@ public class GameManager : Singleton<GameManager> {
         }
     }
 
-    private IEnumerator AwaitTurn() {
+    private IEnumerator AwaitTurn()
+    {
         allowInput = false;
         yield return new WaitForSeconds(1);
-        switch (state) {
+        switch (state)
+        {
             case GameState.Player1:
                 player1.SetVisible(false);
                 break;
@@ -83,7 +122,8 @@ public class GameManager : Singleton<GameManager> {
         }
 
         yield return new WaitForSeconds(1);
-        switch (state) {
+        switch (state)
+        {
             case GameState.Player1:
                 state = GameState.Player2;
                 player2.SetVisible(true);
@@ -97,20 +137,25 @@ public class GameManager : Singleton<GameManager> {
         allowInput = true;
     }
 
-    private void Start() {
+    private void Start()
+    {
         player1 = new Field(this);
         player2 = new Field(this);
         //player1.SetVisible(tr);
         player2.SetVisible(false);
     }
 
-    public Field CurrentField {
-        get {
-            if (state == GameState.Player1) {
+    public Field CurrentField
+    {
+        get
+        {
+            if (state == GameState.Player1)
+            {
                 return player1;
             }
 
-            if (state == GameState.Player2) {
+            if (state == GameState.Player2)
+            {
                 return player2;
             }
 
--- Assets/Scripts/Structure.cs
+++ Assets/Scripts/Structure.cs
@@ -1,7 +1,31 @@
 using System;
 
 [Serializable]
-public struct Structure {
+public struct Structure
+{
     public byte width, height;
     public bool[] composition;
+
+    public bool CanPlaceIn(Field field, int baseX, int baseY)
+    {
+        if (baseX + width >= field.width)
+        {
+            return false;
+        }
+        if (baseY + height >= field.height)
+        {
+            return false;
+        }
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                if (field.Grid[baseX + x, baseY + y].unit != null)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
